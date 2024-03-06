2fedb24419732e2617e5386e26fe56209f313d3d
Mathis Beauville <mathis.beauville@gmail.com>
2024-03-06 14:56:53 +0100

    feat: add

--- pizza.py
+++ pizza.py
@@ -1,5 +1,6 @@
 class Pizza:
-    def __init__(self, nom, ingredients, prix):
+    def __init__(self, nom, ingredients, prix, base):
         self.nom = nom
         self.ingredients = ingredients
         self.prix = prix
+        self.base = base
